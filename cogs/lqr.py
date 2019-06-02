d0dda6c065022db6e403c02d0f983cbbc8682514
Joseph Banks <joseph@josephbanks.me>
2019-06-02 01:10:24 +0100

    executor

--- cogs/lqr.py
+++ cogs/lqr.py
@@ -22,6 +22,31 @@ class LiquidRescale(commands.Cog):
         async with self.session.get(url) as response:
             return await response.read()
 
+    def liquid_rescale(self, image_data):
+        with Image(blob=image_data) as im:
+
+            if im.height > 500:
+                rescale_factor = 500 / im.width
+                im.resize(int(im.width * rescale_factor),
+                          int(im.height * rescale_factor))
+            elif im.width > 500:
+                rescale_factor = 500 / im.width
+                im.resize(int(im.width * rescale_factor),
+                          int(im.height * rescale_factor))
+
+            original_height = im.height
+            original_width = im.width
+
+            im.liquid_rescale(width=int(im.width * 0.5),
+                              height=int(im.height * 0.5),
+                              delta_x=1, rigidity=0)
+
+            im.resize(original_width, original_height)
+
+            binary = im.make_blob('png')
+
+        return binary
+
     @commands.Cog.cog_unload
     def unload(self):
         self.bot.loop.create_task(self.session.close())
@@ -61,28 +86,13 @@ class LiquidRescale(commands.Cog):
 
         storage = BytesIO()
 
-        with Image(blob=image_data) as im:
+        binary = self.bot.loop.run_in_executor(
+            None,
+            self.liquid_rescale,
+            image_data
+        )
 
-            if im.height > 500:
-                rescale_factor = 500 / im.width
-                im.resize(int(im.width * rescale_factor),
-                          int(im.height * rescale_factor))
-            elif im.width > 500:
-                rescale_factor = 500 / im.width
-                im.resize(int(im.width * rescale_factor),
-                          int(im.height * rescale_factor))
-
-            original_height = im.height
-            original_width = im.width
-
-            im.liquid_rescale(width=int(im.width * 0.5),
-                              height=int(im.height * 0.5),
-                              delta_x=1, rigidity=0)
-
-            im.resize(original_width, original_height)
-
-            binary = im.make_blob('png')
-            storage.write(binary)
+        storage.write(binary)
 
         await rescaling_message.delete()
 
