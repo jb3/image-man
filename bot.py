ee563c880eb8c8db9b933a855cb0a9d5687d93f1
Joseph Banks <joseph@josephbanks.me>
2019-06-02 15:03:32 +0100

    big boy error handler

--- bot.py
+++ bot.py
@@ -25,12 +25,6 @@ class ImageMan(commands.Bot):
         log.info(f"Logged in as {self.user.name}")
         await self.load_cogs()
 
-    async def on_command_error(self, ctx, exception):
-        await self.log_error(
-            exception.original.args[0],
-            title=exception.original.__class__.__name__
-        )
-
     async def log_error(self, exception, title="Error"):
         error_embed = Embed(title=title, color=Color.red())
         error_embed.description = exception
@@ -54,6 +48,57 @@ class ImageMan(commands.Bot):
                 await self.log_error(f"{e.name}: {e.args[0]}",
                                      title="Could not load cog")
                 log.error(f"[-] Could not load {module}")
+
+    async def on_command_error(self, ctx, error):
+        # Try provide some user feedback instead of logging all errors.
+
+        if isinstance(error, commands.CommandNotFound):
+            return  # No need to unknown commands anywhere or return feedback
+
+        if isinstance(error, commands.MissingRequiredArgument):
+            # Missing arguments are likely human error so do not need logging
+            parameter_name = error.param.name
+            return await ctx.send(f"\N{NO ENTRY SIGN} Required argument "
+                                  f"{parameter_name} was missing")
+        elif isinstance(error, commands.CheckFailure):
+            return await ctx.send("\N{NO ENTRY SIGN} You do not have "
+                                  "permission to use that command")
+        elif isinstance(error, commands.CommandOnCooldown):
+            retry_after = round(error.retry_after)
+            return await ctx.send(f"\N{HOURGLASS} Command is on cooldown, try"
+                                  f"again after {retry_after} seconds")
+
+        # All errors below this need reporting and so do not return
+
+        if isinstance(error, commands.ArgumentParsingError):
+            # Provide feedback & report error
+            await ctx.send("\N{NO ENTRY SIGN} An issue occurred while"
+                           "attempting to parse an argument")
+        elif isinstance(error, commands.BadArgument):
+            await ctx.send("\N{NO ENTRY SIGN} Conversion of an argument"
+                           "failed")
+        else:
+            await ctx.send("\N{NO ENTRY SIGN} An error occured during "
+                           "execution, the error has been reported.")
+
+        extra_context = {
+            "discord_info": {
+                "Channel": ctx.channel.mention,
+                "User": ctx.author.mention,
+                "Command": ctx.message.content
+            }
+        }
+
+        if ctx.guild is not None:
+            # We are NOT in a DM
+            extra_context["discord_info"]["Message"] = (
+                f'[{ctx.message.id}](https://discordapp.com/channels/'
+                f'{ctx.guild.id}/{ctx.channel.id}/{ctx.message.id})'
+            )
+        else:
+            extra_context["discord_info"]["Message"] = f"{ctx.message.id} (DM)"
+
+        self.bot.log.exception(error, extra=extra_context)
 
 
 if __name__ == "__main__":
